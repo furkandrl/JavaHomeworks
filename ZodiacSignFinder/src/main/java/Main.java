81acefb9e6f1e12a7953bd255cdaf425843cb14e
Furkan Dereli <frkndereli@gmail.com>
2022-11-04 21:53:24 +0300

    project done

---
+++ ZodiacSignFinder/src/main/java/Main.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int month, day;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Doğduğunuz ay (sayı olarak): ");
+        month = in.nextInt();
+        System.out.println("Doğduğunuz gün (sayı olarak): ");
+        day = in.nextInt();
+
+        if(month == 1){
+            if(day >= 1 && day <= 31){
+            if(day < 22){
+                System.out.println("Oğlak");
+                }else{
+                System.out.println("Kova");
+            }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 2){
+            if(day >= 1 && day <=29){
+                if(day < 20){
+                    System.out.println("Kova");
+                }else{
+                    System.out.println("Balık");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+
+        }else if(month == 3){
+            if(day >= 1 && day <=31){
+                if(day < 21){
+                    System.out.println("Balık");
+                }else{
+                    System.out.println("Koç");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 4){
+            if(day >= 1 && day <=31){
+                if(day < 21){
+                    System.out.println("Koç");
+                }else{
+                    System.out.println("Boğa");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 5){
+            if(day >= 1 && day <=31){
+                if(day <22){
+                    System.out.println("Boğa");
+                }else{
+                    System.out.println("İkizler");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 6){
+            if(day >= 1 && day <=31){
+                if(day <23){
+                    System.out.println("İkizler");
+                }else{
+                    System.out.println("Yangeç");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 7){
+            if(day >= 1 && day <=31){
+                if(day <23){
+                    System.out.println("Yengeç");
+                }else{
+                    System.out.println("Aslan");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 8){
+            if(day >= 1 && day <=31){
+                if(day <23){
+                    System.out.println("Aslan");
+                }else{
+                    System.out.println("Başak");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 9){
+            if(day >= 1 && day <=31){
+                if(day <23){
+                    System.out.println("Başak");
+                }else{
+                    System.out.println("Terazi");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 10){
+            if(day >= 1 && day <=31){
+                if(day <23){
+                    System.out.println("Terazi");
+                }else{
+                    System.out.println("Akrep");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 11){
+            if(day >= 1 && day <=31){
+                if(day <22){
+                    System.out.println("Akrep");
+                }else{
+                    System.out.println("Yay");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else if(month == 12){
+            if(day >= 1 && day <=31){
+                if(day <22){
+                    System.out.println("Yay");
+                }else{
+                    System.out.println("Oğlak");
+                }
+            }else{
+                System.out.println("Geçersiz gün girdiniz.");
+            }
+        }else{
+            System.out.println("Geçersiz ay girdiniz");
+        }
+
+    }
+}
