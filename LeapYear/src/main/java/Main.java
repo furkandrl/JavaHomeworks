2589a870c214b0b5773ffbb256e008b54c183258
Furkan Dereli <frkndereli@gmail.com>
2022-11-05 15:06:29 +0300

    project done

---
+++ LeapYear/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Year: ");
+        int year = in.nextInt();
+
+        if(year % 100 == 0 && year % 4 == 0){
+            //System.out.println(year+" is a leap year.");
+            if(year % 400 == 0){
+                System.out.println(year+" is a leap year.");
+            }else{
+                System.out.println(year+" is not a leap year.");
+            }
+        }else if(year % 4 == 0){
+            System.out.println(year+" is a leap year.");
+        }else{
+            System.out.println(year+" is not a leap year.");
+        }
+    }
+}
